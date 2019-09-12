0ed3c21f97935b4a9c2a3261bfd2831106a0221b
Thomas Graft <mr@thomasgraft.com>
2019-09-12 17:17:49 -0400

    Cleanup + favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ class Header extends Component {
   render() {
     return (
       <header className="avenir tc pv4 pv5-ns">
-        <img src="http://tachyons.io/img/logo.jpg" className="br-100 pa1 ba b--black-10 h3 w3" alt="avatar" />
+        <img className="br-100 pa1 ba b--black-10 h3 w3" alt="avatar" 
+             src="https://www.gravatar.com/avatar/f797f30baabc306e01069cd718cdb6ed" />
         <h1 className="f3 f4-ns fw6 mid-gray">Tom Graft</h1>
         <h2 className="f6 gray fw2 ttu tracked">
           Full Stack Web Developer<br />
